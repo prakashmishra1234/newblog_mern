e6078472971e60cc9fbfe90fc9a520bc6f758d93
Prakash Mishra <prakash@sflhub.comgit config --global user.email prakash@sflhub.com>
2023-04-06 18:38:22 +0530

    navbar changed

--- client/src/components/layout/Sidebar.js
+++ client/src/components/layout/Sidebar.js
@@ -8,8 +8,14 @@ import ListItem from "@mui/material/ListItem";
 import ListItemButton from "@mui/material/ListItemButton";
 import ListItemIcon from "@mui/material/ListItemIcon";
 import ListItemText from "@mui/material/ListItemText";
+import HomeOutlinedIcon from "@mui/icons-material/HomeOutlined";
+import { Routeconstant } from "../routing/Routeconstant";
+import { Link } from "react-router-dom";
+import { AuthContext } from "../../store/store";
 
 export default function Sidebar({ state, setState }) {
+  const context = React.useContext(AuthContext);
+
   return (
     <React.Fragment>
       <SwipeableDrawer
@@ -46,46 +52,138 @@ export default function Sidebar({ state, setState }) {
           <List>
             <ListItem disablePadding>
               <ListItemButton>
-                <ListItemIcon></ListItemIcon>
-                <ListItemText primary={"Home"} />
+                <Link
+                  style={{
+                    textDecoration: "none",
+                    display: "flex",
+                    margin: "0 1rem",
+                  }}
+                  to={Routeconstant.HOME}
+                  onClick={() => {
+                    setState(false);
+                  }}
+                >
+                  <ListItemIcon>
+                    <HomeOutlinedIcon />
+                  </ListItemIcon>
+                  <ListItemText primary={"Home"} />
+                </Link>
               </ListItemButton>
             </ListItem>
             <ListItem disablePadding>
               <ListItemButton>
-                <ListItemIcon></ListItemIcon>
-                <ListItemText primary={"About"} />
+                <Link
+                  style={{
+                    textDecoration: "none",
+                    display: "flex",
+                    margin: "0 1rem",
+                  }}
+                  to={Routeconstant.ABOUT}
+                  onClick={() => {
+                    setState(false);
+                  }}
+                >
+                  <ListItemIcon></ListItemIcon>
+                  <ListItemText primary={"About"} />
+                </Link>
               </ListItemButton>
             </ListItem>
             <ListItem disablePadding>
               <ListItemButton>
-                <ListItemIcon></ListItemIcon>
-                <ListItemText primary={"Create"} />
+                <Link
+                  style={{
+                    textDecoration: "none",
+                    display: "flex",
+                    margin: "0 1rem",
+                  }}
+                  to={Routeconstant.CREATE}
+                  onClick={() => {
+                    setState(false);
+                  }}
+                >
+                  <ListItemIcon></ListItemIcon>
+                  <ListItemText primary={"Create"} />
+                </Link>
               </ListItemButton>
             </ListItem>
             <ListItem disablePadding>
               <ListItemButton>
-                <ListItemIcon></ListItemIcon>
-                <ListItemText primary={"Donate"} />
+                <Link
+                  style={{
+                    textDecoration: "none",
+                    display: "flex",
+                    margin: "0 1rem",
+                  }}
+                  to={Routeconstant.DONATE}
+                  onClick={() => {
+                    setState(false);
+                  }}
+                >
+                  <ListItemIcon></ListItemIcon>
+                  <ListItemText primary={"Donate"} />
+                </Link>
               </ListItemButton>
             </ListItem>
-            <ListItem disablePadding>
-              <ListItemButton>
-                <ListItemIcon></ListItemIcon>
-                <ListItemText primary={"Users"} />
-              </ListItemButton>
-            </ListItem>
-            <ListItem disablePadding>
-              <ListItemButton>
-                <ListItemIcon></ListItemIcon>
-                <ListItemText primary={"Login"} />
-              </ListItemButton>
-            </ListItem>
-            <ListItem disablePadding>
-              <ListItemButton>
-                <ListItemIcon></ListItemIcon>
-                <ListItemText primary={"Signup"} />
-              </ListItemButton>
-            </ListItem>
+            {context.userData.role.includes("admin") ? (
+              <ListItem disablePadding>
+                <ListItemButton>
+                  <Link
+                    style={{
+                      textDecoration: "none",
+                      display: "flex",
+                      margin: "0 1rem",
+                    }}
+                    to={Routeconstant.USER}
+                    onClick={() => {
+                      setState(false);
+                    }}
+                  >
+                    <ListItemIcon></ListItemIcon>
+                    <ListItemText primary={"Users"} />
+                  </Link>
+                </ListItemButton>
+              </ListItem>
+            ) : null}
+            {context.isLogin ? null : (
+              <>
+                <ListItem disablePadding>
+                  <ListItemButton>
+                    <Link
+                      style={{
+                        textDecoration: "none",
+                        display: "flex",
+                        margin: "0 1rem",
+                      }}
+                      to={Routeconstant.LOGIN}
+                      onClick={() => {
+                        setState(false);
+                      }}
+                    >
+                      <ListItemIcon></ListItemIcon>
+                      <ListItemText primary={"Login"} />
+                    </Link>
+                  </ListItemButton>
+                </ListItem>
+                <ListItem disablePadding>
+                  <ListItemButton>
+                    <Link
+                      style={{
+                        textDecoration: "none",
+                        display: "flex",
+                        margin: "0 1rem",
+                      }}
+                      to={Routeconstant.SIGNUP}
+                      onClick={() => {
+                        setState(false);
+                      }}
+                    >
+                      <ListItemIcon></ListItemIcon>
+                      <ListItemText primary={"Sign up"} />
+                    </Link>
+                  </ListItemButton>
+                </ListItem>
+              </>
+            )}
           </List>
         </Box>
       </SwipeableDrawer>
---
+++ client/src/customHooks/useFetch.js
@@ -0,0 +1,25 @@
+import axios from "axios";
+import { useState, useEffect } from "react";
+
+const useFetch = (reqObj) => {
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState("");
+
+  useEffect(() => {
+    axios(reqObj)
+      .then((res) => {
+        setData(res.data);
+      })
+      .catch((err) => {
+        setError(err);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  }, []);
+
+  return { data, loading, error };
+};
+
+export default useFetch;
